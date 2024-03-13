2f3ed2c367a0e9993654ca7cd169a397ac74f5f4
Logeshwaran21 <logeshwaran.a@diggibyte.com>
2024-03-13 14:48:26 +0530

    mutations- string slicing,UDF,loggers

---
+++ src/mutations/util.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format="%(message)s" )
+
+def mutate_string(string, position, character):
+
+    string_input = string[:position] + character + string[position + 1:]
+    logging.debug(string_input)
+    return string_input
