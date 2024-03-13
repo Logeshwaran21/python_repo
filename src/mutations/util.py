5a6bb067ed9db403565b2cd01aac0190fbe9024c
Logeshwaran21 <logeshwaran.a@diggibyte.com>
2024-03-13 15:29:17 +0530

    mutations using UDF,string_slicing,loggers

--- src/mutations/util.py
+++ src/mutations/util.py
@@ -1,9 +1,6 @@
 import logging
-
 logging.basicConfig(level=logging.DEBUG, format="%(message)s" )
-
 def mutate_string(string, position, character):
-
     string_input = string[:position] + character + string[position + 1:]
     logging.debug(string_input)
     return string_input
