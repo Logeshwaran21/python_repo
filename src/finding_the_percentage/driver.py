4008c8b8457f194409923ab4c1b04cc32e06459b
Logeshwaran21 <logeshwaran.a@diggibyte.com>
2024-03-13 06:28:12 +0530

    finding the percentage using UDF, Map

--- src/finding_the_percentage/driver.py
+++ src/finding_the_percentage/driver.py
@@ -1,1 +1,8 @@
+from util import read_student_marks, calculate_average
+student_marks = read_student_marks()
+query_name = input("Enter the name of the student to calculate average: ")
+# Call function from util.py to calculate average score
+average_score = calculate_average(student_marks, query_name)
+# Print the average score with two decimal places
+print(f"The average of {query_name} is {average_score:.2f}")
 
--- src/finding_the_percentage/util.py
+++ src/finding_the_percentage/util.py
@@ -1,1 +1,13 @@
+def read_student_marks():
+    n = int(input("Enter the number of students? "))
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input("Enter the name of the student and their marks: ").split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    return student_marks
 
+def calculate_average(student_marks, query_name):
+    # Calculate average score for the queried student
+    average_score = sum(student_marks[query_name]) / len(student_marks[query_name])
+    return average_score
