6eb0f4510c77c8f33bc1bd143425bd61d4730b15
Logeshwaran21 <logeshwaran.a@diggibyte.com>
2024-03-13 21:41:55 +0530

    merge_the_tools usig UDF,loggers

--- src/merge_the_tools/driver.py
+++ src/merge_the_tools/driver.py
@@ -2,7 +2,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format="%(message)s" )
 from util import merge_tools
 
-string_input= (input("Enter the string value: "))
+string_input = input("Enter the string value: ")
 k = int(input("Enter the factor value: "))
 
 merge_tools(string_input,k)
--- src/merge_the_tools/util.py
+++ src/merge_the_tools/util.py
@@ -2,15 +2,18 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format="%(message)s" )
 def merge_tools(string_input,k):
     n= len(string_input)
+    res=""
     for i in range(0,n,k):
         sub_string = string_input[i:i+k]
         unique_list=[]
         for j in sub_string:
             if j not in unique_list:
                 unique_list.append(j)
+                res+=j
+        res+="\n"
         result=(''.join(unique_list))
-        logging.debug(result)
-    return result
+        logging.debug(f"The output is {result}")
+    return res
 
 
 
