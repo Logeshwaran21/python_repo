3f5775efa7ca38593882d3a1156b58e56b9bb1e0
Logeshwaran21 <logeshwaran.a@diggibyte.com>
2024-03-14 20:20:30 +0530

    test_pilling

--- test/pilling_up/test_pilling.py
+++ test/pilling_up/test_pilling.py
@@ -1,8 +1,20 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+from python_repo.src.pilling_up.util import main,can_stack_cubes
+
+class TestCanStackCubes(unittest.TestCase):
+    def test_can_stack_cubes_yes(self):
+        cubes = [1, 2, 3]
+        self.assertEqual(can_stack_cubes(3, cubes), "Yes")
+
+    def test_can_stack_cubes_no(self):
+        cubes = [3, 2, 1, 4]
+        self.assertEqual(can_stack_cubes(4, cubes), "No")
+
+    def test_can_stack_cubes_empty(self):
+        cubes = []
+        self.assertEqual(can_stack_cubes(0, cubes), "Yes")
 
 if __name__ == '__main__':
     unittest.main()
+
