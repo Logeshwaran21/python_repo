b7606d7cdbe8a12c97872166acb1ba184abf6f0b
Logeshwaran21 <logeshwaran.a@diggibyte.com>
2024-03-14 20:19:18 +0530

    pilling UDF,while, If-else

---
+++ src/pilling_up/util.py
@@ -0,0 +1,31 @@
+def can_stack_cubes(num_cubes, cube_lengths):
+    left = 0
+    right = num_cubes - 1
+    prev_cube = float('inf')
+
+    while left <= right:
+        if cube_lengths[left] > cube_lengths[right]:
+            current_cube = cube_lengths[left]
+            left += 1
+        else:
+            current_cube = cube_lengths[right]
+            right -= 1
+
+        if current_cube > prev_cube:
+            return "No"
+
+        prev_cube = current_cube
+
+    return "Yes"
+
+
+def main():
+    num_test_cases = int(input().strip())
+
+    for _ in range(num_test_cases):
+        num_cubes = int(input().strip())
+        cube_lengths = list(map(int, input().strip().split()))
+        print(can_stack_cubes(num_cubes, cube_lengths))
+
+
+
