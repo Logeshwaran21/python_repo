e84b8034bafc3ff5e614eac1e0488e080acfbc54
Logeshwaran21 <logeshwaran.a@diggibyte.com>
2024-03-18 07:28:14 +0530

    UDF, String-slicing,loggers

--- src/mutations/driver.py
+++ src/mutations/driver.py
@@ -1,7 +1,3 @@
-import logging
 from python_repo.src.mutations.util import mutate_string
-logging.basicConfig(level=logging.DEBUG, format="%(message)s" )
 
-s = input("Enter the string: ")
-i, c = input("enter the position and character: ").split()
-s_new = mutate_string(s, int(i), c)
+mutate_string()
--- src/mutations/util.py
+++ src/mutations/util.py
@@ -1,6 +1,8 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format="%(message)s" )
-def mutate_string(string, position, character):
+def mutate_string():
+    string = input("Enter the string: ")
+    position, character = input("enter the position and character: ").split()
     string_input = string[:position] + character + string[position + 1:]
     logging.debug(string_input)
     return string_input
