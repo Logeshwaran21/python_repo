428f44515d8ea0c28712fe8c16f93349868aead6
Logeshwaran21 <logeshwaran.a@diggibyte.com>
2024-03-18 07:05:27 +0530

    Calendar time, date

--- src/calendar_module/driver.py
+++ src/calendar_module/driver.py
@@ -1,5 +1,3 @@
-import logging
 from util import calendar_module
-logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 
 calendar_module()
--- src/calendar_module/util.py
+++ src/calendar_module/util.py
@@ -5,7 +5,9 @@ logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 
 def calendar_module():
     date_str = input("Enter a date (MM-DD-YEAR): ")
+
     month, day, year = map(int, date_str.split('-'))
     day_of_week = datetime.date(year,month,day).weekday()
     days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    logging.debug(f"The day on, {date_str}, is, {days_of_week[day_of_week]}")
+    logging.debug(days_of_week[day_of_week])
+    return days_of_week[day_of_week]
