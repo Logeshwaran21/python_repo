51e51ea35709b10aca7bf984d192c249c91d99e0
Logeshwaran21 <logeshwaran.a@diggibyte.com>
2024-03-18 07:12:15 +0530

    finding_the_percentage, UDF, loggers, list- comprehension,

--- src/finding_the_percentage/driver.py
+++ src/finding_the_percentage/driver.py
@@ -1,10 +1,3 @@
-import logging
 from util import read_student_marks, calculate_average
-
-logging.basicConfig(level=logging.DEBUG, format="%(message)s" )
-
-student_marks = read_student_marks()
-query_name = input("Enter the name of the student to calculate average: ")
-average_score = calculate_average(student_marks, query_name)
-logging.info(f"The average of {query_name} is {average_score:.2f}")
+read_student_marks()
 
