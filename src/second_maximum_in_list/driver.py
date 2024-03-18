8e96076c9573ce4c5a6cd14579ac59e1dd4c4c95
Logeshwaran21 <logeshwaran.a@diggibyte.com>
2024-03-18 07:34:06 +0530

    UDF, loggers, For, list-comprehension

--- src/second_maximum_in_list/driver.py
+++ src/second_maximum_in_list/driver.py
@@ -1,6 +1,3 @@
 from python_repo.src.second_maximum_in_list.util import second_maximum
-import logging
-logging.basicConfig(level=logging.DEBUG, format="%(message)s")
-num= int(input("Enter the number of values: "))
-List_Input = list(map(int, input("Enter the values: ").split()))
-logging.debug(f"The second_maximum in the list is {second_maximum(num,List_Input)}")
+
+second_maximum()
--- src/second_maximum_in_list/util.py
+++ src/second_maximum_in_list/util.py
@@ -1,7 +1,8 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 def second_maximum(n,List):
-
+    num = int(input("Enter the number of values: "))
+    List_Input = list(map(int, input("Enter the values: ").split()))
     for i in range(len(List)):
         for j in range(i + 1, len(List)):
             if List[i] > List[j]:
