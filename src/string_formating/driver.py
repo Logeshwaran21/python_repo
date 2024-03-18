bf3f91e76d408c210cbeb763a589ea1d78269e21
Logeshwaran21 <logeshwaran.a@diggibyte.com>
2024-03-18 07:39:27 +0530

    list-comprehension

--- src/string_formating/driver.py
+++ src/string_formating/driver.py
@@ -1,4 +1,3 @@
 from util import string_formatting
-k=int(input("Enter the number: "))
-sf=string_formatting(k)
-print(sf)
+
+string_formatting()
--- src/string_formating/util.py
+++ src/string_formating/util.py
@@ -1,6 +1,16 @@
-def string_formatting(k):
-    de,oc,he,bi=[[str(i) for i in range(1, k + 1)],[oct(i)[2:] for i in range(1, k + 1)],[hex(i)[2:] for i in range(1, k + 1)],[bin(i)[2:] for i in range(1, k + 1)]]
-    res=""
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+
+def string_formatting():
+    k = int(input("Enter the number: "))
+    de = [str(i) for i in range(1, k + 1)]
+    oc = [oct(i)[2:] for i in range(1, k + 1)]
+    he = [hex(i)[2:].upper() for i in range(1, k + 1)]
+    bi = [bin(i)[2:] for i in range(1, k + 1)]
+
+    res = ""
     for i in range(k):
-        res += de[i] + "\t\t" + oc[i] + "\t\t" + he[i].upper() + "\t\t" + bi[i] + "\n"
+        res += f"{de[i]:>2}{' ':>5}{oc[i]:>3}{' ':>5}{he[i]:>3}{' ':>5}{bi[i]:>3}\n"
+    logging.debug(res)
     return res
